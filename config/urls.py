0ba9cca133dc0238387a4f45e5161dea4f0d6bc5
usbpendrive <ferry.fernando@gmail.com>
2018-06-06 16:25:49 +0700

    Add sitemaps.xml

--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,18 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls import url
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts': PostSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap'),
 ]
 
 if settings.DEBUG:
